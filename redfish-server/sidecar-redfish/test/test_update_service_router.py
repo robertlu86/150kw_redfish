dad7687abef8f7b544f7cdeaed64f4cc5b25c2c2
motwicemo <motwicemo2266@gmail.com>
2025-06-27 13:59:53 +0800

    0627 1.修正EventService/Subscriptions/id超出上限 2.修正ethernetinterfaces state的狀態

--- redfish-server/sidecar-redfish/test/test_update_service_router.py
+++ redfish-server/sidecar-redfish/test/test_update_service_router.py
@@ -1,3 +1,14 @@
+'''
+## Step1: Run python http.server at port 5300
+$ cd test/
+$ python -m http.server 5300 --bind 127.0.0.1
+
+## Step2: Test your server
+$ curl 'http://127.0.0.1:5300/testcase-updateservice-example.zip.gpg' -o test.gpg
+$ wget 'http://127.0.0.1:5300/testcase-updateservice-example.zip.gpg'
+
+## Step3: Run pytest
+'''
 import os
 import json
 import pytest
@@ -8,8 +19,9 @@ test_root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(test_root)
 from .conftest import print_response_details
 
-
-endpoint = "/redfish/v1/UpdateService/Actions/UpdateCdu.SimpleUpdate"
+image_domain_path = "127.0.0.1:5300/testcase-updateservice-example.zip.gpg"
+image_url = f"http://{image_domain_path}"
+endpoint = "/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate"
 
 updateservice_testcases = [
     {
@@ -23,42 +35,182 @@ updateservice_testcases = [
             # },
             # "Actions": {
             #     "#UpdateService.SimpleUpdate": {
-            #         "target": "/redfish/v1/UpdateService/Actions/UpdateCdu.SimpleUpdate",
+            #         "target": "/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate",
             #         "@Redfish.ActionInfo": "/redfish/v1/UpdateService/SimpleUpdateActionInfo"
             #     }
             # }
         }
     },
+    # {
+    #     "endpoint": "/redfish/v1/UpdateService/FirmwareInventory",
+    #     "assert_cases": { 
+    #         "@odata.id": "/redfish/v1/UpdateService/FirmwareInventory",
+    #         "@odata.type": "#SoftwareInventoryCollection.SoftwareInventoryCollection",
+    #         "Name": "Firmware Inventory",
+    #         "Members@odata.count": 2,
+    #         "Members": [
+    #             {
+    #                 "@odata.id": "/redfish/v1/UpdateService/FirmwareInventory/System_Software"
+    #             },
+    #             {
+    #                 "@odata.id": "/redfish/v1/UpdateService/FirmwareInventory/Control_Firmware"
+    #             }
+    #         ],
+    #     }
+    # },
+]
+
+updateservice_firmwareinventory_testcases = [
     {
-        "endpoint": "/redfish/v1/UpdateService/FirmwareInventory",
+        "endpoint": "/redfish/v1/UpdateService/FirmwareInventory/Control_Firmware",
         "assert_cases": { 
-            "@odata.id": "/redfish/v1/UpdateService/FirmwareInventory",
-            "@odata.type": "#SoftwareInventoryCollection.SoftwareInventoryCollection",
-            "Name": "Firmware Inventory",
-            "Members@odata.count": 1,
-            "Members": [
-                {
-                    "@odata.id": "/redfish/v1/UpdateService/FirmwareInventory/WebInterface"
-                }
-            ],
+            "@odata.id": "/redfish/v1/UpdateService/FirmwareInventory/Control_Firmware",
+            "@odata.type": "#SoftwareInventory.v1_12_0.SoftwareInventory",
+            "Updateable": False,
+            "Version": "0103", # MUST equals to the display version of webUI plc_version (prefix: '0')
+            "SoftwareId": "PLC-VERSION"
         }
     },
     {
-        "endpoint": "/redfish/v1/UpdateService/FirmwareInventory/WebInterface",
+        "endpoint": "/redfish/v1/UpdateService/FirmwareInventory/System_Software",
         "assert_cases": { 
-            "@odata.id": "/redfish/v1/UpdateService/FirmwareInventory/WebInterface",
-            "@odata.type": "#SoftwareInventory.v1_3_0.SoftwareInventory",
-            "Name": "Web Interface firmware",
+            "@odata.id": "/redfish/v1/UpdateService/FirmwareInventory/System_Software",
+            "@odata.type": "#SoftwareInventory.v1_12_0.SoftwareInventory",
             "Updateable": True,
-            # "Version": "0103",
+            "Version": "0117", # MUST equals to the display version of webUI version
             "SoftwareId": "WEB-INTERFACE"
+        }
+    },
+    
+]
+
+updateservice_simpleupdate_by_imageuri_testcases = [
+    {
+        "endpoint": f"/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate",
+        "payload": {
+            "ImageURI": image_url
+        },
+        "assert_cases": { 
+        }
+    },
+    {
+        "endpoint": f"/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate",
+        "payload": {
+            "ImageURI": image_url,
+            "TransferProtocol": "HTTP"
+        },
+        "assert_cases": { 
+        }
+    },
+
+    {
+        "endpoint": f"/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate",
+        "payload": {
+            "ImageURI": image_domain_path,
+            "TransferProtocol": "HTTP"
+        },
+        "assert_cases": { 
         }
     },
 ]
 
+updateservice_simpleupdate_by_imageuri_fail_testcases = [
+    {
+        "endpoint": f"/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate",
+        "payload": {
+            "ImageURI": image_url,
+            "TransferProtocol": "http" # only Upper case allowed in Redfish
+        },
+        "assert_cases": { 
+        }
+    },
+    {
+        "endpoint": f"/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate",
+        "payload": {
+            "ImageURI": image_domain_path,
+            "TransferProtocol": "http://"
+        },
+        "assert_cases": { 
+        }
+    },
+    {
+        "endpoint": f"/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate",
+        "payload": {
+            "ImageURI": image_domain_path,
+            "TransferProtocol": "hTTP"
+        },
+        "assert_cases": { 
+        }
+    },
+    {
+        "endpoint": f"/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate",
+        "payload": {
+            "ImageURI": image_domain_path,
+            "TransferProtocol": "FTP"
+        },
+        "assert_cases": { 
+        }
+    },
+    {
+        "endpoint": f"/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate",
+        "payload": {
+            "ImageURI": image_domain_path,
+            "TransferProtocol": "ftp"
+        },
+        "assert_cases": { 
+        }
+    },
+    {
+        "endpoint": f"/redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate",
+        "payload": {
+            "ImageURI": image_domain_path,
+            "TransferProtocol": "://"
+        },
+        "assert_cases": { 
+        }
+    },
+]
+
+@pytest.mark.parametrize('testcase', updateservice_simpleupdate_by_imageuri_testcases)
+def test_update_service_SimpleUpdate_by_ImageUri(client, basic_auth_header, testcase):
+    """[TestCase] /redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate (by ImageUri)"""
+    
+    print(f"POST {testcase['endpoint']}")
+    print(f"Payload: {testcase['payload']}")
+    
+    t1 = time.time()
+    response = client.post(
+        testcase['endpoint'],
+        headers=basic_auth_header,
+        json=testcase['payload'],
+    )
+    t2 = time.time()
+
+    print_response_details(response)
+
+    assert response.status_code == 200 # 200代表上傳&更新韌體成功 (這邊是sync.的設計)
+    assert t2 - t1 < 120 # 120秒內完成
+    print(f"PASS: POST {testcase['endpoint']} with payload {testcase['payload']} is expected to return 200")
+
+@pytest.mark.parametrize('testcase', updateservice_simpleupdate_by_imageuri_fail_testcases)
+def test_update_service_SimpleUpdate_by_ImageUri_fail(client, basic_auth_header, testcase):
+    """[TestCase] /redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate (by ImageUri) fail"""
+    print(f"POST {testcase['endpoint']}")
+    print(f"Payload: {testcase['payload']}")
+    
+    response = client.post(
+        testcase['endpoint'],
+        headers=basic_auth_header,
+        json=testcase['payload'],
+    )
+    
+    print_response_details(response)
+
+    assert response.status_code != 200
+    print(f"PASS: POST {testcase['endpoint']} with payload {testcase['payload']} is NOT return 200")
 
 def test_update_service_upload(client, basic_auth_header):
-    """[TestCase] update_service API: /redfish/v1/UpdateService/Actions/UpdateCdu.SimpleUpdate"""
+    """[TestCase] update_service API: /redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate"""
     print(f"Endpoint: {endpoint}")
     
     file = None
@@ -88,7 +240,7 @@ def test_update_service_upload(client, basic_auth_header):
     assert t2 - t1 < 120 # 120秒內完成
     
 def test_update_service_upload_noPassword(client, basic_auth_header):
-    """[TestCase] update_service API: /redfish/v1/UpdateService/Actions/UpdateCdu.SimpleUpdate (noPassword)
+    """[TestCase] update_service API: /redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate (noPassword)
     Expect:
         Http status: 400
         Response json:
@@ -120,7 +272,7 @@ def test_update_service_upload_noPassword(client, basic_auth_header):
     assert response.status_code == 400
     
 def test_update_service_upload_noSpareDir(client, basic_auth_header):
-    """[TestCase] update_service API: /redfish/v1/UpdateService/Actions/UpdateCdu.SimpleUpdate (noSpareDir)"""
+    """[TestCase] update_service API: /redfish/v1/UpdateService/Actions/UpdateService.SimpleUpdate (noSpareDir)"""
     print(f"Endpoint: {endpoint}")
     
     file = None
@@ -167,3 +319,25 @@ def test_updateservice_api(client, basic_auth_header, testcase):
             assert resp_json["Status"]["Health"] in ["OK", "Warning", "Critical"]
         else:
             assert resp_json[key] == value
+
+@pytest.mark.parametrize('testcase', updateservice_firmwareinventory_testcases)
+def test_updateservice_firmwareinventory_api(client, basic_auth_header, testcase):
+    """[TestCase] updateservice firmwareinventory API"""
+    print(f"GET {testcase['endpoint']}")
+    response = client.get(testcase['endpoint'], headers=basic_auth_header)
+    print_response_details(response)
+    assert response.status_code == 200
+    
+    resp_json = response.json
+    for key, value in testcase['assert_cases'].items():
+        # Version比較特殊，因為webUI的plc_version會補0，但version不會補0
+        if key == "SoftwareId":
+            if value == "WEB-INTERFACE":
+                assert resp_json["Version"] != ""
+                print(f"PASS: Version({resp_json['Version']}) is expected to be non-empty")
+            elif value == "PLC-VERSION":
+                assert resp_json["Version"].startswith("0")
+                print(f"PASS: Version({resp_json['Version']}) is expected to start with 0")
+        else:
+            assert resp_json[key] == value
+            print(f"PASS: resp_json[{key}] is expected to be {value}, actual: {resp_json[key]}")
